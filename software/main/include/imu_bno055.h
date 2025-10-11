488b0ff467b79ee24d20486aca8ab1be91b0399f
Remma Matsuda <titech.renma@gmail.com>
2025-10-11 18:35:03 +0900

    imu refactoring done.

--- software/main/include/imu_bno055.h
+++ software/main/include/imu_bno055.h
@@ -9,17 +9,18 @@ class imu_bno055 {
 
     void setup();
     void get_attitude_data(float data[3]);
-    void get_accel_data(float data[3]);
     void get_angvel_data(float data[3]);
-    void calibrate();
-    void printIMUEvent(sensors_event_t* event);
+    void get_accel_data(float data[3]);
     void emergency_stop(Arm &arm);
 
   private:
+    template<typename T> bool is_nan_or_inf(const std::vector<T> &arr);
+    void calibrate();
+    float clamp_deg(float value);
+
     Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire);
     bool initialized_ = false;
     std::vector<float> initial_attitude_data_ = {0.0f, 0.0f, 0.0f};
     std::vector<float> attitude_data_ = {0.0f, 0.0f, 0.0f};
     std::vector<float> angvel_data_ = {0.0f, 0.0f, 0.0f};
-    template<typename T> bool is_nan_or_inf(const std::vector<T> &arr);
 };
--- software/main/src/imu_bno055.cpp
+++ software/main/src/imu_bno055.cpp
@@ -8,39 +8,47 @@ void imu_bno055::setup() {
   {
     Serial.println("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
   }
-  //calibrate();
+}
+
+float imu_bno055::clamp_deg(float value) {
+  while (value >  180.0) value -= 360.0;
+  while (value < -180.0) value += 360.0;
+
+  return value;
+}
+
+template<typename T> bool imu_bno055::is_nan_or_inf(const std::vector<T> &arr) {
+  for (size_t i = 0; i < arr.size(); i++) {
+    if (std::isnan(arr[i]) || std::isinf(arr[i])) {
+      return true;
+    }
+  }
+  return false;
 }
 
 void imu_bno055::get_attitude_data(float data[3]) {
   sensors_event_t orientationData;
   bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
 
-  double raw_roll_deg  = -orientationData.orientation.z;
-  double raw_pitch_deg = -orientationData.orientation.y;
-  double raw_yaw_deg   = -orientationData.orientation.x;
-  auto clamp = [](float value) -> double {
-    while (value >  180.0) value -= 360.0;
-    while (value < -180.0) value += 360.0;
-    return value;
-  };
-  constexpr float offset_roll_deg = 0.0;  //-5.0;
-  constexpr float offset_pitch_deg = 0.0;
-  data[0] = clamp(raw_roll_deg) - initial_attitude_data_[0] + offset_roll_deg;    // deg
-  data[1] = clamp(raw_pitch_deg) - initial_attitude_data_[1] + offset_pitch_deg ; // deg
-  data[2] = clamp(raw_yaw_deg) - initial_attitude_data_[2];   // deg
-
+  float raw_roll_deg  = -orientationData.orientation.z;
+  float raw_pitch_deg = -orientationData.orientation.y;
+  float raw_yaw_deg   = -orientationData.orientation.x;
   if (!initialized_) {
-    initial_attitude_data_[0] = clamp(raw_roll_deg);
-    initial_attitude_data_[1] = clamp(raw_pitch_deg);
-    initial_attitude_data_[2] = clamp(raw_pitch_deg);
+    initial_attitude_data_[0] = clamp_deg(raw_roll_deg);
+    initial_attitude_data_[1] = clamp_deg(raw_pitch_deg);
+    initial_attitude_data_[2] = clamp_deg(raw_pitch_deg);
     initialized_ = true;
   }
-  attitude_data_[0] = data[0];
-  attitude_data_[1] = data[1];
-  attitude_data_[2] = data[2];
+
+  constexpr float offset_roll_deg = 0.0;
+  constexpr float offset_pitch_deg = 0.0;
+  constexpr float offset_yaw_deg = 0.0;
+  attitude_data_[0] = data[0] = clamp_deg(raw_roll_deg) - initial_attitude_data_[0] + offset_roll_deg;
+  attitude_data_[1] = data[1] = clamp_deg(raw_pitch_deg) - initial_attitude_data_[1] + offset_pitch_deg ;
+  attitude_data_[2] = data[2] = clamp_deg(raw_yaw_deg) - initial_attitude_data_[2] + offset_yaw_deg;
 
 #ifdef DEBUG_IMU_ATTITUDE
-  Serial.print("Euler: ");
+  Serial.print("Euler orientation roll, pitch, yaw: ");
   Serial.print(data[0]);
   Serial.print(" ");
   Serial.print(data[1]);
@@ -69,12 +77,9 @@ void imu_bno055::get_accel_data(float data[3]) {
 void imu_bno055::get_angvel_data(float data[3]) {
   sensors_event_t angVelocityData;
   bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
-  data[0] = angVelocityData.gyro.x * RAD_TO_DEG;
-  data[1] = angVelocityData.gyro.y * RAD_TO_DEG;
-  data[2] = angVelocityData.gyro.z * RAD_TO_DEG;
-  angvel_data_[0] = data[0];
-  angvel_data_[1] = data[1];
-  angvel_data_[2] = data[2];
+  angvel_data_[0] = data[0] = angVelocityData.gyro.x * RAD_TO_DEG;
+  angvel_data_[1] = data[1] = angVelocityData.gyro.y * RAD_TO_DEG;
+  angvel_data_[2] = data[2] = angVelocityData.gyro.z * RAD_TO_DEG;
 
 #ifdef DEBUG_IMU_ANGVEL
   Serial.print("Gyro: ");
@@ -91,56 +96,6 @@ void imu_bno055::calibrate() {
   bno.getCalibration(&system, &gyro, &accel, &mag);
 }
 
-void imu_bno055::printIMUEvent(sensors_event_t* event) {
-  Serial.print("IMU data: ");
-  double x = -1000000, y = -1000000,
-         z = -1000000;  // dumb values, easy to spot problem
-  if (event->type == SENSOR_TYPE_ACCELEROMETER) {
-    Serial.print("Accl:");
-    x = event->acceleration.x;
-    y = event->acceleration.y;
-    z = event->acceleration.z;
-  } else if (event->type == SENSOR_TYPE_ORIENTATION) {
-    Serial.print("Orient:");
-    x = event->orientation.x;
-    y = event->orientation.y;
-    z = event->orientation.z;
-  } else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
-    Serial.print("Mag:");
-    x = event->magnetic.x;
-    y = event->magnetic.y;
-    z = event->magnetic.z;
-  } else if (event->type == SENSOR_TYPE_GYROSCOPE) {
-    Serial.print("Gyro:");
-    x = event->gyro.x;
-    y = event->gyro.y;
-    z = event->gyro.z;
-  } else if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
-    Serial.print("Rot:");
-    x = event->gyro.x;
-    y = event->gyro.y;
-    z = event->gyro.z;
-  } else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
-    Serial.print("Linear:");
-    x = event->acceleration.x;
-    y = event->acceleration.y;
-    z = event->acceleration.z;
-  } else if (event->type == SENSOR_TYPE_GRAVITY) {
-    Serial.print("Gravity:");
-    x = event->acceleration.x;
-    y = event->acceleration.y;
-    z = event->acceleration.z;
-  } else {
-    Serial.print("Unknown data type:");
-  }
-  Serial.print("\tx= ");
-  Serial.print(x);
-  Serial.print(" |\ty= ");
-  Serial.print(y);
-  Serial.print(" |\tz= ");
-  Serial.println(z);
-}
-
 void imu_bno055::emergency_stop(Arm &arm) {
   if (std::abs(attitude_data_[0]) > 70.0f || std::abs(attitude_data_[1]) > 70.0f) {
     arm.set_arm_status(false);
@@ -149,13 +104,4 @@ void imu_bno055::emergency_stop(Arm &arm) {
   if (is_nan_or_inf(attitude_data_) || is_nan_or_inf(angvel_data_)) {
     arm.set_arm_status(false);
   }
-}
-
-template<typename T> bool imu_bno055::is_nan_or_inf(const std::vector<T> &arr) {
-  for (size_t i = 0; i < arr.size(); i++) {
-    if (std::isnan(arr[i]) || std::isinf(arr[i])) {
-      return true;
-    }
-  }
-  return false;
 }
